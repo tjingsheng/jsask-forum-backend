75d982924e7c535d738c044b5f4d7cb493bfc3c6
Jsh3ng <tanjs41200@gmail.com>
2022-12-25 00:42:14 +0800

    Add new routes

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -5,13 +5,27 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
-	retriever "github.com/tjingsheng/jsask-forum-backend/internal/handlers"
+	"github.com/tjingsheng/jsask-forum-backend/internal/handlers"
 )
 
 func GetRoutes() func(r chi.Router) {
 	return func(r chi.Router) {
+		r.Get("/posts", func(w http.ResponseWriter, req *http.Request) {
+			response, _ := handlers.HandleListPosts(w, req)
+
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(response)
+		})
+
 		r.Get("/users", func(w http.ResponseWriter, req *http.Request) {
-			response, _ := retriever.HandleList(w, req, "users")
+			response, _ := handlers.HandleListUsers(w, req)
+
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(response)
+		})
+
+		r.Get("/tags", func(w http.ResponseWriter, req *http.Request) {
+			response, _ := handlers.HandleListTags(w, req)
 
 			w.Header().Set("Content-Type", "application/json")
 			json.NewEncoder(w).Encode(response)
