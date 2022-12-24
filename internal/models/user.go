4dab6f64258aa82c1e8de57432a4c50c84c6e0a1
Jsh3ng <tanjs41200@gmail.com>
2022-12-24 21:28:36 +0800

    Fix model for user

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,12 +1,7 @@
 package models
 
-import "fmt"
-
 type User struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
-}
-
-func (user *User) Greet() string {
-	return fmt.Sprintf("Hello, I am %s", user.Name)
+	User_ID       int    `json:"userId"`
+	Username      string `json:"username"`
+	User_Datetime string `json:"userDatetime"`
 }
