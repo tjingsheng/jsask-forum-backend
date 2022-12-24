a1383a17a39e93187c2859359dd7215317005e0d
Jsh3ng <tanjs41200@gmail.com>
2022-12-24 21:31:15 +0800

    Abstract handleList

--- go.mod
+++ go.mod
@@ -4,5 +4,4 @@ go 1.18
 
 require (
 	github.com/go-chi/chi v1.5.4
-	github.com/pkg/errors v0.9.1
 )
---
+++ internal/handlers/retriever.go
@@ -0,0 +1,45 @@
+package retriever
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"errors"
+
+	"github.com/tjingsheng/jsask-forum-backend/internal/api"
+	users "github.com/tjingsheng/jsask-forum-backend/internal/dataaccess"
+	"github.com/tjingsheng/jsask-forum-backend/internal/database"
+)
+
+const (
+	SuccessfulListTargetMessage = "Successfully listed %s"
+	ErrRetrieveDatabase         = "Failed to retrieve database. Therefore fail to retrieve %s in %s.HandleList"
+	ErrRetrieveTarget           = "Failed to retrieve %s in %s.HandleList"
+	ErrEncodeView               = "Failed to retrieve %s in %s.HandleList"
+)
+
+func HandleList(w http.ResponseWriter, r *http.Request, target string) (*api.Response, error) {
+	db, err := database.GetDB()
+
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf(ErrRetrieveDatabase, target, target))
+	}
+
+	users, err := users.List(db)
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf(ErrRetrieveTarget, target, target))
+	}
+
+	data, err := json.Marshal(users)
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf(ErrEncodeView, target, target))
+	}
+
+	return &api.Response{
+		Payload: api.Payload{
+			Data: data,
+		},
+		Messages: []string{fmt.Sprintf(SuccessfulListTargetMessage, target)},
+	}, nil
+}
