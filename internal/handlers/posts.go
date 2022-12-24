3f4d9cb76ad8f30d3530030af26b8c6ac6af496e
Jsh3ng <tanjs41200@gmail.com>
2022-12-25 00:40:59 +0800

    Fix import bugs :)

---
+++ internal/handlers/posts.go
@@ -0,0 +1,37 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/tjingsheng/jsask-forum-backend/internal/api"
+	"github.com/tjingsheng/jsask-forum-backend/internal/dataaccess"
+	"github.com/tjingsheng/jsask-forum-backend/internal/database"
+)
+
+func HandleListPosts(w http.ResponseWriter, r *http.Request) (*api.Response, error) {
+	db, err := database.GetDB()
+
+	if err != nil {
+		return nil, fmt.Errorf(ErrRetrieveDatabase, ListPosts)
+	}
+
+	users, err := dataaccess.ListPosts(db)
+
+	if err != nil {
+		return nil, fmt.Errorf(ErrRetrieveUsers, ListPosts)
+	}
+
+	data, err := json.Marshal(users)
+	if err != nil {
+		return nil, fmt.Errorf(ErrEncodeView, ListPosts)
+	}
+
+	return &api.Response{
+		Payload: api.Payload{
+			Data: data,
+		},
+		Messages: []string{SuccessfulListMessage},
+	}, nil
+}
