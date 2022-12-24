3fc012427c3f7f53189a0458f89dc63daaf81ccc
Jsh3ng <tanjs41200@gmail.com>
2022-12-24 21:40:11 +0800

    Revert changes

--- internal/handlers/retriever.go
+++ internal/handlers/retriever.go
@@ -1,4 +1,4 @@
-package retriever
+package users
 
 import (
 	"encoding/json"
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -5,13 +5,13 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
-	"github.com/tjingsheng/jsask-forum-backend/internal/handlers/retriever"
+	"github.com/tjingsheng/jsask-forum-backend/internal/handlers/users"
 )
 
 func GetRoutes() func(r chi.Router) {
 	return func(r chi.Router) {
 		r.Get("/users", func(w http.ResponseWriter, req *http.Request) {
-			response, _ := retriever.HandleList(w, req, "users")
+			response, _ := users.HandleList(w, req, "users")
 
 			w.Header().Set("Content-Type", "application/json")
 			json.NewEncoder(w).Encode(response)
