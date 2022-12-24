831836bd13470a8790a7012281947f871b3b1f21
Jsh3ng <tanjs41200@gmail.com>
2022-12-24 21:30:44 +0800

    Fix sample data

--- internal/dataaccess/users.go
+++ internal/dataaccess/users.go
@@ -8,16 +8,34 @@ import (
 func List(db *database.Database) ([]models.User, error) {
 	users := []models.User{
 		{
-			ID:   1,
-			Name: "Susan",
+			User_ID:       1,
+			Username:      "Bobby Lee One",
+			User_Datetime: "2014-03-12T13:37:27+00:00",
 		},
 		{
-			ID:   2,
-			Name: "Bobby",
+			User_ID:       2,
+			Username:      "Bobby Lee Two",
+			User_Datetime: "2014-03-12T13:37:27+00:00",
 		},
 		{
-			ID:   3,
-			Name: "Harry",
+			User_ID:       3,
+			Username:      "Bobby Lee Three",
+			User_Datetime: "2014-03-12T13:37:27+00:00",
+		},
+		{
+			User_ID:       4,
+			Username:      "Harry Potter One",
+			User_Datetime: "2014-03-12T13:37:27+00:00",
+		},
+		{
+			User_ID:       5,
+			Username:      "Bobby Lee Five",
+			User_Datetime: "2014-03-12T13:37:27+00:00",
+		},
+		{
+			User_ID:       6,
+			Username:      "Harry Potter Two",
+			User_Datetime: "2014-03-12T13:37:27+00:00",
 		},
 	}
 	return users, nil
