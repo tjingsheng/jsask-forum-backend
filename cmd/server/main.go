575585d4c2b1da4a97a84509bc2085a58beedb7c
Jsh3ng <tanjs41200@gmail.com>
2022-12-24 17:49:52 +0800

    Fix fmt print message

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	r := router.Setup()
-	fmt.Print("Listening on port 8000 at http://localhost:8000!")
+	fmt.Print("Listening on port 8000 at http://localhost:8000 !")
 
 	log.Fatalln(http.ListenAndServe(":8000", r))
 }
