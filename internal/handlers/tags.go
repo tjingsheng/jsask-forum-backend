b64052c4de02c8873e89dfa8aed0fea207128461
Jsh3ng <tanjs41200@gmail.com>
2023-01-09 13:36:30 +0800

    Remove old code from tags.go handler

--- internal/handlers/tags.go
+++ internal/handlers/tags.go
@@ -10,22 +10,10 @@ import (
 )
 
 func GetAllTags(w http.ResponseWriter, r *http.Request) (*api.Response, error) {
-
 	tags, err := dataaccess.ListTags(database.DB)
-
 	allTagsView := make([]tagsview.ListView, len(tags))
 	for i := range tags {
 		allTagsView[i] = tagsview.ListFrom(tags[i])
 	}
-
-	// keys := make(map[tagsview.ListView]bool)
-	// uniqueTags := []tagsview.ListView{}
-	// for _, i := range allTagsView {
-	// 	if _, obj := keys[i]; !obj {
-	// 		keys[i] = true
-	// 		uniqueTags = append(uniqueTags, i)
-	// 	}
-	// }
-
 	return format(err, allTagsView, ListTags)
 }
