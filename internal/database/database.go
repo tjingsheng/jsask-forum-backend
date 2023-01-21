12c6a6292eab5f2031625d50e327b9ae70b79be7
Jsh3ng <tanjs41200@gmail.com>
2023-01-21 23:59:48 +0800

    Add stuff

--- internal/database/database.go
+++ internal/database/database.go
@@ -4,8 +4,6 @@ import (
 	"log"
 	"os"
 
-	"github.com/joho/godotenv"
-
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -13,9 +11,12 @@ import (
 var DB = InitDB()
 
 func InitDB() *gorm.DB {
-	DB := goDotEnvVariable("DB")
-	DB_USER := goDotEnvVariable("DB_USER")
-	DB_PASS := goDotEnvVariable("DB_PASS")
+	DB := os.Getenv("DB")
+	DB_USER := os.Getenv("DB_USER")
+	DB_PASS := os.Getenv("DB_PASS")
+	// DB := goDotEnvVariable("DB")
+	// DB_USER := goDotEnvVariable("DB_USER")
+	// DB_PASS := goDotEnvVariable("DB_PASS")
 	dbURL := "postgres://" + DB_USER + ":" + DB_PASS + "@" + DB + "/JsaskDatabase"
 	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
 
@@ -26,12 +27,12 @@ func InitDB() *gorm.DB {
 	return db
 }
 
-func goDotEnvVariable(key string) string {
-	err := godotenv.Load(".env")
+// func goDotEnvVariable(key string) string {
+// 	err := godotenv.Load(".env")
 
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
+// 	if err != nil {
+// 		log.Fatalf("Error loading .env file")
+// 	}
 
-	return os.Getenv(key)
-}
+// 	return os.Getenv(key)
+// }
