0882882813bd38dc1f9baf8befd93f2fec4458ff
tjingsheng <tanjs41200@gmail.com>
2025-03-19 03:26:34 +0800

    configure for azure deployment

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/go-chi/chi"
 	"github.com/tjingsheng/jsask-forum-backend/internal/routes"
 )
 
 func main() {
-	fmt.Print("Server is running on http://localhost:8000")
-	log.Fatal(http.ListenAndServe(":8000", routes.Router()))
+	port := "8000"
+	if val, ok := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT"); ok {
+		port = val
+	}
+
+	r := chi.NewRouter()
+
+	r.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			log.Printf("Received request: %s %s", r.Method, r.URL.Path)
+			next.ServeHTTP(w, r)
+		})
+	})
+
+	log.Printf("Server is running on http://localhost:%s\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, routes.Router()))
 }
