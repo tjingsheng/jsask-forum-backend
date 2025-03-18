0a5a7b17ca7ba74b2e1cc80208e5001ad41f3925
tjingsheng <tanjs41200@gmail.com>
2025-03-18 16:56:37 +0800

    hardcode port in server main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,14 +4,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/tjingsheng/jsask-forum-backend/internal/router"
 )
 
 func main() {
-	port := os.Getenv("PORT")
 	r := router.Setup()
-	fmt.Print("Listening on: " + port)
-	log.Fatal(http.ListenAndServe(":"+port, r))
+	fmt.Print("Server is running on http://localhost:3000")
+	log.Fatal(http.ListenAndServe(":3000", r))
 }
